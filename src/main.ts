cb629127866ab022fe83da584c915615c4a1035d
OksanaNevolina <onevolina5gmail.com>
2025-02-26 13:30:53 +0200

    Added swagger and  exception-filter in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/exeptions/global-exception.filter';
+import { SwaggerHelper } from './common/helpers/swagger.helper';
+import { AppConfig, Config } from './configs/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('PLATFORM-S API ')
+    .setDescription('API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'headers',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerHelper.setDefaultResponses(document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defauntModelExpandDepth: 3,
+      persistAuthorization: true,
+    },
+  });
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService<Config>);
+  const appConfig = configService.get<AppConfig>('app');
+  console.log(appConfig);
+  await app.listen(appConfig.port, () => {
+    const url = `http://${appConfig.host}:${appConfig.port}`;
+    Logger.log(`Server running ${url}`);
+    Logger.log(`Swagger running ${url}/docs`);
+  });
 }
 bootstrap();
